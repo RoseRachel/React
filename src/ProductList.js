79e40d3a43078b4c2f773acdd6b0e05ed136398a
Student <roserachel818@gmail.com>
2020-08-28 11:29:08 +0300

    addedproducts

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,28 +1,42 @@
 
-   
 import React from 'react';
 import ProductAlerts from './ProductAlerts';
-
-
-
 import {products} from './products';
 
+
+// function UserGreeting(props) {
+//   return<h1>Welcome back!</h1>;
+// }
+//   function GuestGreeting(props){
+//     return<h1>Please sign up</h1>
+// }
+
+// function greeting (props){
+//   const isLoggedIn = props.isLOggedIn;
+//   if (isLoggedIn){
+//     return < UserGreeting/>;
+
+//   }
+//   return <GuestGreeting/>;
+// }
 
 const ProductList = ({ name }) => {
   const Buy = () => {
     window.alert('Added to cart');
   }
 
-  const onNotify = () => {
-    window.alert('You will be notified when the product goes on sale');
-  }
+  // const onNotify = () => {
+  //   window.alert('You will be notified when the product goes on sale');
+  // }
 
   return (
     <div className="product-list">
       <h2>My Groceries</h2>
-
+     
+            {/* <ProductAlerts product={product} notify={onNotify} /> */}
       {products.map(product => {
         return (
+      
           <div class="products">
             <h3>
               <a title={`${product.type1} details`}>
@@ -43,10 +57,12 @@ const ProductList = ({ name }) => {
             </h3>
             {product.veges && <p>{`Type: ${product.veges}`}</p>}
             {product.fruits && <p>{`Type: ${product.fruits}`}</p>}
+            {product.cereals && <p>{`Type: ${product.cereals}`}</p>}
+            {product.herbs && <p>{`Type: ${product.herbs}`}</p>}
             <button onClick={Buy}>
               Buy
             </button>
-            <ProductAlerts product={product} notify={onNotify} />
+            {/* <ProductAlerts product={product} notify={onNotify} /> */}
             
           </div>
         )
@@ -56,4 +72,3 @@ const ProductList = ({ name }) => {
 };
 
 export default ProductList;
-
